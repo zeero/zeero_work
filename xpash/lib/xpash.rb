681a9430516b4b66305581c0063048abce0459da
zeero <zeero26@gmail.com>
2011-02-25 11:17:07 +0900

    Migrate to gem structure generated by newgem.

--- xpash/lib/xpash.rb
+++ xpash/lib/xpash.rb
@@ -1,9 +1,14 @@
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
 require 'logger'
 
 require 'rubygems'
 require 'nokogiri'
 
 class XPash
+
+  VERSION = '0.0.1'
 
   DEFAULT_PATH = "//"
   ROOT_PATH = "/"
