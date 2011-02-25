dfd414c41a3a4b271e96206c4c7957e8a6a4d5f8
zeero <zeero26@gmail.com>
2011-02-25 20:07:26 +0900

    A few changes about method name.

--- xpash/test/test_xpash.rb
+++ xpash/test/test_xpash.rb
@@ -11,22 +11,22 @@ class TestXPash < Test::Unit::TestCase
   end
 
   def test_cd
-    cd_common("h3", 5, "//h3")
+    asserts_cd("h3", 5, "//h3")
   end
 
-  def test_cd_plural
-    cd_common("div", 3, "//div")
-    cd_common("article", 1, "//div/article")
-    cd_common("..", 3, "//div")
+  def test_cd_multiple
+    asserts_cd("div", 3, "//div")
+    asserts_cd("article", 1, "//div/article")
+    asserts_cd("..", 3, "//div")
   end
 
   def test_cd_abspath
-    cd_common("/html/head/link", 3, "/html/head/link")
+    asserts_cd("/html/head/link", 3, "/html/head/link")
     
-    cd_common("//div//li", 3, "//div//li")
+    asserts_cd("//div//li", 3, "//div//li")
   end
 
-  def cd_common(input, expect_return, expect_query)
+  def asserts_cd(input, expect_return, expect_query)
     assert_equal(expect_return, @xpash.cd(input))
     assert_equal(expect_query, @xpash.query)
     if /^.+?:\d+(?::in `(.*)')?/ =~ caller.first
