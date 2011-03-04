946dc80765f6dac97cdabe5893500f239b919bfc
zeero <zeero26@gmail.com>
2011-03-04 17:07:38 +0900

    Create RSpec code.

--- xpash/spec/xpash_spec.rb
+++ xpash/spec/xpash_spec.rb
@@ -1,11 +1,32 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-# Time to add your specs!
-# http://rspec.info/
-describe "Place your specs here" do
-  
-  it "find this spec in spec directory" do
-    # violated "Be sure to write your specs"
+describe XPash, 'cd command' do
+  before do
+    @xpash = XPash.new("#{File.dirname(__FILE__)}/../test/test.html")
   end
   
+  it 'should return number of found elements.' do
+    @xpash.cd("div").should == 3
+  end
+
+  it 'should add "/" and argument string to @query.' do
+    @xpash.cd("div")
+    @xpash.query.should == "//div"
+  end
+
 end
+  
+describe XPash, 'cd command, when input starts with ".."' do
+  before do
+    @xpash = XPash.new("#{File.dirname(__FILE__)}/../test/test.html")
+  end
+
+  it 'should update @query to upper path' do
+    @xpash.cd("div")
+    @xpash.cd("article")
+    @xpash.cd("..").should == 3
+    @xpash.query.should == "//div"
+  end
+
+end
+
